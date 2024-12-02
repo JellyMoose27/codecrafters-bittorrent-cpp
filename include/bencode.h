17f94cc6db4dad10389b238395897e36066794d4
JellyMoose27 <jellymoosethefishgoose@gmail.com>
2024-12-02 18:41:10 +0700

    updated

---
+++ include/bencode.h
@@ -0,0 +1,14 @@
+#ifndef BENCODE_H
+#define BENCODE_H
+
+#include "lib/nlohmann/json.hpp"
+
+using json = nlohmann::json;
+
+class Bencode
+{
+public:
+    json decode(const std::string& encoded_value);
+};
+
+#endif // BENCODE_H
