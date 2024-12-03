80f8e07cecd8d84af7e7669b2acec1bc1522d144
JellyMoose27 <jellymoosethefishgoose@gmail.com>
2024-12-03 21:28:48 +0700

    updated

--- src/Main.cpp
+++ src/Main.cpp
@@ -8,93 +8,80 @@
 
 #include <cstdlib>
 
+#include <fstream>
+
 #include "lib/nlohmann/json.hpp"
 
 using json = nlohmann::json;
 
 
-
-// json decode_bencoded_string(const std::string& encoded_value) {
-
-//     size_t colon_index = encoded_value.find(':');
-
-//     if (colon_index != std::string::npos) {
-
-//         std::string number_string = encoded_value.substr(0, colon_index);
-
-//         int64_t number = std::atoll(number_string.c_str());
-
-//         std::string str = encoded_value.substr(colon_index + 1, number);
-
-//         return json(str);
-
-//     } else {
-//         throw std::runtime_error("Invalid encoded value: " + encoded_value);
-//     }
-
-// }
-
-// json decode_bencoded_integer(const std::string& encoded_value, size_t& index) {
-//     size_t end_index = encoded_value.find('e', index);
-//     if (end_index != std::string::npos) {
-
-//         std::string num = encoded_value.substr(index + 1, end_index - index - 1);
-
-//         int64_t number = std::atoll(num.c_str());
-//         index = end_index + 1;
-
-//         return json(number);
-
-//     } else {
-
-//         throw std::runtime_error("Invalid encoded value: " + encoded_value);
-
-//     }
-
-// }
-
-// json decode_bencoded_list(const std::string& encoded_value, size_t& index) {
-//     std::vector<json> list;
-
-//     while(index < encoded_value.size() - 1)
-//     {
-//         if (encoded_value[index] == 'e')
-//         {
-//             index++;
-//             return list;
-//         }
-//         if (std::isdigit(encoded_value[index]))
-//         {
-//             size_t colon_index = encoded_value.find(':');
-//             std::string number_string = encoded_value.substr(index, colon_index);
-
-//             int64_t number = std::atoll(number_string.c_str());
-
-//             std::string str = encoded_value.substr(colon_index + 1, number);
-//             list.push_back(json(str));
-//             index = colon_index + number + 1;
-//         }
-//         else if (encoded_value[index] == 'l')
-//         {
-//             index++;
-//             list.push_back(decode_bencoded_list(encoded_value, index));
-//         }
-//         else
-//         {
-//             size_t end_idx = encoded_value.find('e', index);
-//             if (end_idx == std::string::npos)
-//                 throw std::invalid_argument("Invalid bencoded integer");
-    
-//             std::string int_s = encoded_value.substr(index + 1, end_idx - index - 1);
-//             list.push_back(json(stoll(int_s)));
-//             index = end_idx + 1;
-//         }
-//     }
-//     index++;
-//     return json(list);
-// }
-
 json decode_bencoded_value(const std::string& encoded_value, size_t& index);
+
+json decode_bencoded_string(const std::string& encoded_value, size_t& index) {
+
+    std::string result = "";
+    while (std::isdigit(encoded_value[index]))
+    {
+        result += encoded_value[index];
+        index++;
+    }
+    int length = std::atoll(result.c_str());
+    result = "";
+    index++;
+    while (length--)
+    {
+        result += encoded_value[index];
+        index++;
+    }
+    return result;
+}
+
+json decode_bencoded_integer(const std::string& encoded_value, size_t& index) {
+    index++;
+    std::string result = "";
+    while(encoded_value[index] != 'e')
+    {
+        result += encoded_value[index];
+        index++;
+    }
+    index++;
+    return json(std::atoll(result.c_str()));
+}
+
+json decode_bencoded_list(const std::string& encoded_value, size_t& index) {
+    index++;
+    json list = json::array();
+    while(encoded_value[index] != 'e')
+    {
+        list.push_back(decode_bencoded_value(encoded_value, index));
+    }
+    index++;
+    return list;
+}
+
+json decode_bencoded_dict(const std::string& encoded_value, size_t& index)
+{
+    index++;
+    json res = json::object();
+    // skip the 'd'
+    while(encoded_value[index] != 'e')
+    {
+        /*
+        d<key1><value1>...<keyN><valueN>
+        Example "d3:foo3:bare"
+        foo is key, bar is value
+
+        lexicographical order: a generalization of the alphabetical order of the dictionaries to sequences of ordered symbols or, 
+        more generally, of elements of a totally ordered set. 
+        */
+        json key = decode_bencoded_value(encoded_value, index);
+        json value = decode_bencoded_value(encoded_value, index);
+        res[key.get<std::string>()] = value;
+    }
+    index++;
+    return res;
+}
+
 
 json decode_bencoded_value(const std::string &encoded_value) {
 
@@ -117,84 +104,22 @@ json decode_bencoded_value(const std::string& encoded_value, size_t& index)
     if (std::isdigit(encoded_value[index]))
     {
         // Example: "5:hello" -> "hello"
-        size_t colon_index = encoded_value.find(':', index);
-
-        if (colon_index != std::string::npos) {
-
-        std::string number_string = encoded_value.substr(index, colon_index - index);
-
-        int64_t number = std::atoll(number_string.c_str());
-
-        std::string str = encoded_value.substr(colon_index + 1, number);
-
-        index = colon_index + number + 1;
-
-        return json(str);
-
-        } else {
-
-            throw std::runtime_error("Invalid encoded value: " + encoded_value);
-
-        }
+        return decode_bencoded_string(encoded_value, index);
     }
     else if (encoded_value[index] == 'i')
     {
         // Example: "i45e" - > "45"
-        size_t e_index = encoded_value.find('e', index);
-
-        if (e_index != std::string::npos) {
-
-        std::string number_string = encoded_value.substr(index + 1, e_index - index - 1);
-
-        int64_t number = std::atoll(number_string.c_str());
-
-        index = e_index + 1;
-
-        return json(number);
-
-        } else {
-
-            throw std::runtime_error("Invalid encoded value: " + encoded_value);
-
-        }
+        return decode_bencoded_integer(encoded_value, index);
     }
     else if (encoded_value[index] == 'l')
     {
-        index++;
         // Example: "l10:strawberryi559ee" -> "[strawberry, 559]"
-        std::vector<json> list;
-        while(encoded_value[index] != 'e') {
-
-            list.push_back(decode_bencoded_value(encoded_value, index));
-
-        }
-
-        index++;
-
-        return json(list);
+        return decode_bencoded_list(encoded_value, index);
     }
     else if (encoded_value[index] == 'd')
     {
-        index++;
         // Example: "d3:foo3:bar5:helloi52ee" -> {"foo":"bar", "hello":"52"}
-        auto dict = nlohmann::ordered_map<json, json>();
-        // skip the 'd'
-        while (encoded_value[index] != 'e')
-        {
-            /*
-            d<key1><value1>...<keyN><valueN>
-            Example "d3:foo3:bare"
-            foo is key, bar is value
-
-            lexicographical order: a generalization of the alphabetical order of the dictionaries to sequences of ordered symbols or, 
-            more generally, of elements of a totally ordered set. 
-            */
-            auto key = decode_bencoded_value(encoded_value, index);
-            auto value = decode_bencoded_value(encoded_value, index);
-            dict.push_back({key, value});
-        }
-        index++;
-        return json(dict);
+        return decode_bencoded_dict(encoded_value, index);
     }
     else
     {
@@ -232,7 +157,16 @@ int main(int argc, char* argv[]) {
         std::cout << decoded_value.dump() << std::endl;
 
     }
-
+    else if (command == "info")
+    {
+        std::string filePath = argv[2];
+        std::ifstream file(filePath, std::ios::binary);
+        std::string fileContent((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+        file.close();
+        json decode_value = decode_bencoded_value(fileContent);
+        std::cout << "Tracker URL: " << decode_value["announce"].get<std::string>() << std::endl;
+        std::cout << "Length: " << decode_value["info"]["length"].get<int>() << std::endl;
+    }
     else {
 
         std::cerr << "unknown command: " << command << std::endl;
