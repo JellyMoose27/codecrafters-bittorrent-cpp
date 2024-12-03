e31bbe61f640e7b07c8ff0b448f025996a5c191b
JellyMoose27 <jellymoosethefishgoose@gmail.com>
2024-12-04 00:30:54 +0700

    updated

--- src/Main.cpp
+++ src/Main.cpp
@@ -376,6 +376,34 @@ int main(int argc, char* argv[]) {
             int length = decoded_torrent["info"]["length"];
 
             // Contruct GET message
+            /*
+            
+            info_hash: the info hash of the torrent
+                20 bytes long, will need to be URL encoded
+                Note: this is NOT the hexadecimal representation, which is 40 bytes long
+
+            peer_id: a unique identifier for your client
+                A string of length 20 that you get to pick.
+
+            port: the port your client is listening on
+                You can set this to 6881, you will not have to support this functionality during this challenge.
+
+            uploaded: the total amount uploaded so far
+                Since your client hasn't uploaded anything yet, you can set this to 0.
+
+            downloaded: the total amount downloaded so far
+                Since your client hasn't downloaded anything yet, you can set this to 0.
+
+            left: the number of bytes left to download
+                Since you client hasn't downloaded anything yet, this'll be the total length of the file
+                (you've extracted this value from the torrent file in previous stages)
+                
+            compact: whether the peer list should use the compact representation
+                For the purposes of this challenge, set this to 1.
+                The compact representation is more commonly used in the wild, 
+                non-compact representation is mostly supported for backward-compatibility.
+
+            */
             std::string peerID = "01234567890123456789";
             std::ostringstream url;
             url << trackerURL << "?info_hash=" << urlEncodedHash
@@ -386,11 +414,29 @@ int main(int argc, char* argv[]) {
                 << "&left=" << length
                 << "&compact=1";
 
+            // Send HTTP GET message (request)
+            // Then receive tracker response
+            /*
+            The tracker's response will be a bencoded dictionary with two keys:
+
+            interval:
+                An integer, indicating how often your client should make a request to the tracker.
+                You can ignore this value for the purposes of this challenge.
+            peers.
+                A string, which contains list of peers that your client can connect to.
+                Each peer is represented using 6 bytes. The first 4 bytes are the peer's IP address
+                and the last 2 bytes are the peer's port number.
+
+            */
             std::string response = http_get(url.str());
 
+            // Decode tracker response
             json trackerResponse = decode_bencoded_value(response);
+
+            // list of peers
             std::string peers = trackerResponse["peers"];
 
+            // parse the peers and print them
             std::vector<std::string> peerList = parse_peers(peers);
             for (const auto& peer : peerList)
             {
